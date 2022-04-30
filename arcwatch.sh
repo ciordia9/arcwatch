4b8f9e8249bece8992ef149af89c206fcec507c3
ciordia9 <andy@nuancelabs.com>
2022-04-29 22:16:00 -0400

    Edit & daemon trial/fail

--- arcwatch.sh
+++ arcwatch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Using inotify toolkit watch a directory and push new files through ArcWelder.
 # Please make sure to install ArcWelderLib from https://github.com/FormerLurker/ArcWelderLib
-# Assign correct paths below
+# Assign correct paths below, remember to install inotify and arcwelder!
 # inotify's logs will go to stderr while ArcWelder's log will go to the directory you specify
 
 arcweldbin=/home/pi/bin/ArcWelder
@@ -9,7 +9,7 @@ watchdir=/home/pi/gcode_files
 logdir=/home/pi/gcode_files/arcweld
 arcdir=/home/pi/gcode_files/arcweld
 
-inotifywait -s -m $watchdir -e create -e moved_to |
+inotifywait -d $watchdir -e create -e moved_to -o /home/pi/dev/arcwatch/log.log|
     while read dir action file; do
 	$arcweldbin $dir/$file $arcdir/aw-$file >> $logdir/arcweld.log
     done
